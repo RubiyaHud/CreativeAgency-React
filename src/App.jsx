9b9aa3e14956bfaf3280a621f76bc49da2466fa4
RubiyaHud <rubiya.hud@gmail.com>
2025-04-06 14:57:56 +0600

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+import Banner from './components/Banner'
+import Header from './components/layouts/Header'
+
+function App() {
+
+  return (
+    <>
+    <Header/>
+    <Banner/>    
+    </>
+  )
+}
+
+export default App
+
+
+
+
---
+++ src/components/Heading.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Heading = ({text, className}) => {
+  return (
+    <h3 className={`${className}`}>
+        {text}
+    </h3>
+  )
+}
+
+export default Heading
