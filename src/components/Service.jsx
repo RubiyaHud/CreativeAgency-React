34a3de7eac26811b4dfa04010671beef76f85ad5
RubiyaHud <rubiya.hud@gmail.com>
2025-04-09 00:46:20 +0600

    Completed Service Part

--- src/components/Service.jsx
+++ src/components/Service.jsx
@@ -1,6 +1,14 @@
 import React from 'react'
 import Container from './Container'
 import Heading from './Heading'
+import Flex from './Flex'
+import ServiceContent from './ServiceContent'
+import UiUxIcon from './icons/UIUXIcon'
+import WebDevIcon from './icons/WebDevIcon'
+import DigMarIcon from './icons/DigMarIcon'
+import GDIcon from './icons/GDIcon'
+import WebDesignIcon from './icons/WebDesignIcon'
+import MotionDesignIcon from './icons/MotionDesignIcon'
 
 const Service = () => {
     return (
@@ -9,6 +17,14 @@ const Service = () => {
                 <Container>
                     <Heading text={"Our Services"} as="h3" className={"font-inter text-menuColor font-bold text-[40px] text-center"} />
                     <p className="font-inter text-menuColor text-[16px] text-center leading-7 opacity-70 w-[542px] m-auto">There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form.</p>
+                    <Flex className={"pt-14 flex-wrap"}>
+                        <ServiceContent Component={UiUxIcon} className={"mr-[30px] mb-[30px]"} headingText={"UI/UX Design"} bodyText={"There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form."} />
+                        <ServiceContent Component={WebDevIcon} className={"mr-[30px] mb-[30px]"} headingText={"Web Development"} bodyText={"There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form."} />
+                        <ServiceContent Component={DigMarIcon} className={"mb-[30px]"} headingText={"Digital Marketing"} bodyText={"There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form."} />
+                        <ServiceContent Component={GDIcon} className={"mr-[30px]"} headingText={"Graphic Design"} bodyText={"There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form."} />
+                        <ServiceContent Component={WebDesignIcon} className={"mr-[30px]"} headingText={"Web Design"} bodyText={"There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form."} />
+                        <ServiceContent Component={MotionDesignIcon} className={""} headingText={"Motion Design"} bodyText={"There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form."} />
+                    </Flex>
                 </Container>
             </div>
         </>
