c14f2fa835bf47164565ac78b31c9e52a9d45004
RubiyaHud <rubiya.hud@gmail.com>
2025-04-08 16:35:18 +0600

    Add Service heading

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@
 import './App.css'
 import Banner from './components/Banner'
 import Header from './components/layouts/Header'
+import Service from './components/Service'
 
 function App() {
 
   return (
     <>
     <Header/>
-    <Banner/>    
+    <Banner/>
+    <Service/>    
     </>
   )
 }
---
+++ src/components/Service.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Container from './Container'
+import Heading from './Heading'
+
+const Service = () => {
+    return (
+        <>
+            <div className="py-[100px]">
+                <Container>
+                    <Heading text={"Our Services"} as="h3" className={"font-inter text-menuColor font-bold text-[40px] text-center"} />
+                    <p className="font-inter text-menuColor text-[16px] text-center leading-7 opacity-70 w-[542px] m-auto">There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form.</p>
+                </Container>
+            </div>
+        </>
+    )
+}
+
+export default Service
