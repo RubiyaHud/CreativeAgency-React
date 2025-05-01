72eb1b8794fe713868bd8fb1ca0a1be704687eaa
RubiyaHud <rubiya.hud@gmail.com>
2025-05-01 08:42:10 +0600

    Add Double Quotation (Customer Experience), Completed the Creative Agency Figma

--- src/components/CustomerExperrience.jsx
+++ src/components/CustomerExperrience.jsx
@@ -5,6 +5,7 @@ import Flex from './Flex'
 import Image from './Image'
 import customerImage from '../assets/customerImg.png'
 import { FaStar } from "react-icons/fa"
+import DoubleQuotation from './icons/DoubleQuotation'
 
 const CustomerExperrience = () => {
     return (
@@ -12,7 +13,7 @@ const CustomerExperrience = () => {
             <Container>
                 <HeadingWithPara className={""} classH={"w-[603px]"} classP={""} hText={"We Care About Our Customer Experience Too"} pText={"There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form."} />
                 <Flex className={"justify-between"}>
-                    <div className={"bg-white mt-12 px-12 pb-6 pt-12 rounded-10 shadow-[0_6px_35px_rgba(0,0,0,0.08)]"}>
+                    <div className={"relative bg-white mt-12 px-12 pb-[46px] pt-12 rounded-10 shadow-[0_6px_35px_rgba(0,0,0,0.08)]"}>
                         <Image imgSrc={customerImage} imgAlt={"Customer Image"} />
                         <Flex className={"gap-1.5 my-7"}>
                             <FaStar className={"text-[20px] text-starColor"} />
@@ -24,8 +25,9 @@ const CustomerExperrience = () => {
                         <p className={"w-[274px] mb-7 font-inter font-medium text-[16px] text-menuColor leading-7 opacity-70"}>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form.</p>
                         <h3 className={"font-inter font-semibold text-[20px] leading-7 text-menuColor"}> Mahid Ahmed </h3>
                         <h3 className={"font-inter font-medium text-[16px] leading-7 text-[#5D5D5D]"}> UX Designer </h3>
+                        <DoubleQuotation className={"absolute right-[30px] bottom-[23px] "}/>
                     </div>
-                    <div className={"bg-white mt-12 px-12 pb-6 pt-12 rounded-10 shadow-[0_6px_35px_rgba(0,0,0,0.08)]"}>
+                    <div className={"relative bg-white mt-12 px-12 pb-[46px] pt-12 rounded-10 shadow-[0_6px_35px_rgba(0,0,0,0.08)]"}>
                         <Image imgSrc={customerImage} imgAlt={"Customer Image"} />
                         <Flex className={"gap-1.5 my-7"}>
                             <FaStar className={"text-[20px] text-starColor"} />
@@ -37,8 +39,9 @@ const CustomerExperrience = () => {
                         <p className={"w-[274px] mb-7 font-inter font-medium text-[16px] text-menuColor leading-7 opacity-70"}>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form.</p>
                         <h3 className={"font-inter font-semibold text-[20px] leading-7 text-menuColor"}> Mahid Ahmed </h3>
                         <h3 className={"font-inter font-medium text-[16px] leading-7 text-[#5D5D5D]"}> UX Designer </h3>
+                        <DoubleQuotation className={"absolute right-[30px] bottom-[23px] "}/>
                     </div>
-                    <div className={"bg-white mt-12 px-12 pb-6 pt-12 rounded-10 shadow-[0_6px_35px_rgba(0,0,0,0.08)]"}>
+                    <div className={"relative bg-white mt-12 px-12 pb-[46px] pt-12 rounded-10 shadow-[0_6px_35px_rgba(0,0,0,0.08)]"}>
                         <Image imgSrc={customerImage} imgAlt={"Customer Image"} />
                         <Flex className={"gap-1.5 my-7"}>
                             <FaStar className={"text-[20px] text-starColor"} />
@@ -50,6 +53,7 @@ const CustomerExperrience = () => {
                         <p className={"w-[274px] mb-7 font-inter font-medium text-[16px] text-menuColor leading-7 opacity-70"}>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form.</p>
                         <h3 className={"font-inter font-semibold text-[20px] leading-7 text-menuColor"}> Mahid Ahmed </h3>
                         <h3 className={"font-inter font-medium text-[16px] leading-7 text-[#5D5D5D]"}> UX Designer </h3>
+                        <DoubleQuotation className={"absolute right-[30px] bottom-[23px] "}/>
                     </div>
                 </Flex>
             </Container>
